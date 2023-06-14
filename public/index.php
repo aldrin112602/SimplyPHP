3a237b341d886ebb461cfeafd98e98d9b0d81954
Aldrin Caballero <aldrincaballero94@gmail.com>
2023-06-14 10:26:54 +0800

    modified:   config/setting.php

--- public/index.php
+++ public/index.php
@@ -3,5 +3,10 @@ require_once '../vendor/autoload.php';
 require_once '../config/setting.php';
 $dotenv = Dotenv\Dotenv::createImmutable( '../' );
 $dotenv->load();
+$config = require_once '../config/config.php';
+
+echo($config['DEV_ENVIRONMENT']  . '<br>');
+echo($config['database']['tests']['hostname']  . '<br>');
+echo($config['database']['tests']['username']  . '<br>');
 
 ?>
