779c83137557aa3b730a8e464d526d77e96d363d
Aldrin Caballero <aldrincaballero94@gmail.com>
2023-06-14 09:07:02 +0800

    modified:   public/index.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,11 @@
 <?php 
 require_once '../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable('../');
-$dotenv->load();
-
+Dotenv\Dotenv::createImmutable('../')->load();
 $config = require_once '../config/config.php';
 
-echo $config['database']['tests']['hostname'];
-echo $config['database']['tests']['username'];
-echo $config['database']['tests']['password'];
+// echo $config['database']['tests']['hostname'];
+// echo $config['database']['tests']['username'];
+// echo $config['database']['tests']['password'];
 
 
 ?>
